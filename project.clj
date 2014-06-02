6a869f4f73cdb11e5da213376abc84586a7b8087
Takashi AOKI <federkasten@me.com>
2014-06-01 23:32:00 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject parcel "0.1.0-SNAPSHOT"
+  :description "A Clojure library for simple message passing library  using RabbitMQ (AMQP)"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.novemberain/langohr "2.11.0"]])
