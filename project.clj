0ce7bc9ee16f7251765e641339ebdf2cd9acbba7
Takashi AOKI <federkasten@me.com>
2015-03-09 05:25:09 +0900

    bump up versions of dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject parcel "0.1.0-SNAPSHOT"
-  :description "A Clojure library for simple message passing library  using RabbitMQ (AMQP)"
+  :description "A simple Clojure library for message passing with RabbitMQ(AMQP)"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.novemberain/langohr "2.11.0"]
+                 [com.novemberain/langohr "3.1.0"]
                  [org.clojure/data.fressian "0.2.0"]])
