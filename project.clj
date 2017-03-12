5eb80eaae1ff3b26c758e159edb416f726aaa89d
Takashi AOKI <me@tak.sh>
2017-03-12 17:16:25 +0900

    version 0.1.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject parcel "0.1.1-SNAPSHOT"
+(defproject parcel "0.1.1"
   :description "Simple Clojure library for message passing with RabbitMQ(AMQP)"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.novemberain/langohr "3.4.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.novemberain/langohr "3.7.0"]
                  [org.clojure/data.fressian "0.2.1"]])
